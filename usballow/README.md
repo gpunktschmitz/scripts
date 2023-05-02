7a142d121b3da04f9853ff8c43cdfeec1e2af275
Guenther Schmitz <727316+gpunktschmitz@users.noreply.github.com>
2023-05-02 09:57:52 +0200

    simplified script to only unblock devices

--- usballow/README.md
+++ usballow/README.md
@@ -1,12 +1,9 @@
 # usballow
 I am using `usbguard` to allow/block usb devices.
 
-If only one devices is filtered/selected it will be toggled (allowed/disabled) - when there are more devices one gets prompted to select which one to be toggled. 
-
-If more than one are present the command `usbguard list-devices --blocked` is executed and expects the device no. to be entered which then will be allowed.
+This script lists all blocked devices and expects the id of the device listed to be entered. It then tries to unblock the device with the given id.
 
 ## my usage
-I use this script on Linux Mint.
-I added the shortcut `CTRl+ALT+U` to the `Keyboard Shortcuts` app executing `mate-terminal -e 'bash /home/myveryusername/scripts/usballow/usballow.sh'` to allow any device not matching the `$filtertext` variable.
-I added the shortcut `CTRL+ALT+C` to the `Keyboard Shortcuts` app executing `mate-terminal -e 'bash /home/myveryusername/scripts/usballow/usballow.sh "EMV Smartcard Reader"'` to toggle only the device matching the `$filterargument` variable.
+I use this script on Linux Mint and added the following shortcut to the app `Keyboard Shortcuts`.
 
+`CTRl+ALT+U` executing `mate-terminal -e 'bash /path/to/script/usballow.sh'`
--- usballow/usballow.sh
+++ usballow/usballow.sh
@@ -1,74 +1,15 @@
 #!/bin/sh
 
-# script takes an argument ($filterargument)
-# if the argument is present the devices are filtered to match that argument
-#   if the filtered devices results in only one device found that devices will be toggled (allowed/blocked)
-# if the argument is not present all blocked devices *but* the "$filtertext" will be listed so that one can be allowed
-filtertext="EMV Smartcard Reader"
+# list only blocked devices: id and device name
+usbguard list-devices --blocked | awk -F\" '{print $1 $4}' | cut -d" " -f1-1,6-
 
-#--
+# get id from user
+read -p "enter id to unblock device: " userinput
 
-red="\e[0;91m"
-green="\e[0;92m"
-reset="\e[0m"
-
-# default action; string to print to console
-actionstring="allow"
-colour=$green
-
-# check if argument given
-if [ $# -eq 0 ]; then
-  devicecount=$(usbguard list-devices --blocked | awk -v pat="$filtertext" -F":" '$0 !~ pat' | wc -l)
-else
-  # if no argument given update set filter variable
-  filterargument=$1
-  devicecount=$(usbguard list-devices | awk -v pat="$filterargument" -F":" '$0 ~ pat' | wc -l)
-fi
-
-# exit if no devices found
-if [ ${devicecount} -eq 0 ]; then 
+# test if userinput variable is empty
+if [ -z "${userinput}" ]; then
   exit
-elif [ ${devicecount} -eq 1 ]; then
-  devicename=$(usbguard list-devices | awk -v pat="$filterargument" -F":" '$0 ~ pat' | grep -o ' name .* hash ' | awk 'BEGIN {FS="\""}{print $2}' | xargs)
-  devicestatus=$(usbguard list-devices | awk -v pat="$filterargument" -F":" '$0 ~ pat' |  cut -d' ' -f2)
-  # if devicestatus is "allow" the device will be blocked; update variable to print to console
-  if [ "${devicestatus}" = "allow" ]; then
-    actionstring="block"
-    colour=$red
-  fi
-
-  # print the action and device name
-  echo -e "${colour}"$(printf "%sing USB device \"%s\"\n" "${actionstring}" "${devicename}")"${reset}"
-
-  if [ $# -eq 0 ]; then
-    sudo usbguard allow-device $(usbguard list-devices --blocked | awk -v pat="$filtertext" -F":" '$0 !~ pat' | awk -F: '{print $1}')
-  else
-    if [ "${devicestatus}" = "allow" ]; then
-      sudo usbguard block-device $(usbguard list-devices | awk -v pat="$filterargument" -F":" '$0 ~ pat' | awk -F: '{print $1}')
-    else
-      sudo usbguard allow-device $(usbguard list-devices | awk -v pat="$filterargument" -F":" '$0 ~ pat' | awk -F: '{print $1}')
-    fi
-  fi
 else
-  # if more than one device was found prompt for the id of the one to be allowed/blocked
-  printf "found %s devices\n" "${devicecount}"
-  if [ $# -eq 0 ]; then
-    usbguard list-devices | awk -v pat="$filtertext" -F":" '$0 !~ pat'
-  else
-    usbguard list-devices | awk -v pat="$filterargument" -F":" '$0 ~ pat'
-  fi
-
-  userinput=""
-  read -p "Input selection (id): " userinput
-
-  if [ -z "$userinput" ]; then
-    exit
-  else
-    devicestatus=$(usbguard list-devices | awk -v pat="$userinput: " -F":" '$0 ~ pat' |  cut -d' ' -f2)
-    if [[ "${devicestatus}" -eq "allow" ]]; then
-      sudo usbguard block-device ${userinput}
-    else
-      sudo usbguard allow-device ${userinput}
-    fi
-  fi
+  # allow device temporary (until device is unplugged or user logs off)
+  sudo usbguard allow-device ${userinput}
 fi
